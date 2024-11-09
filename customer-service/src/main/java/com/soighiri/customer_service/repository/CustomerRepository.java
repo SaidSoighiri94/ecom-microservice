d4f78a3ad73387b00e22389ea2dc179e629506c2
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-11-09 23:55:36 +0100

    Configuration de la base de données et les loggs

--- customer-service/src/main/java/com/soighiri/customer_service/repository/CustomerRepository.java
+++ customer-service/src/main/java/com/soighiri/customer_service/repository/CustomerRepository.java
@@ -1,8 +1,10 @@
 package com.soighiri.customer_service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.soighiri.customer_service.entities.Customer;
+@RepositoryRestResource
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
 
