4c4dad48543bac26d576b8bed09aa416057799c7
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-11-20 00:13:49 +0100

    Creation d'une DTO Customer

--- billing-service/src/main/java/com/soighiri/billing_service/entities/Bill.java
+++ billing-service/src/main/java/com/soighiri/billing_service/entities/Bill.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,6 +23,9 @@ public class Bill {
 	private Long id;
 	private Date billingDate;
 	private long customerId;
+	@OneToMany(mappedBy = "bill")
 	private List<ProductItem> productItems = new ArrayList<>();
+	
+	@Transient private Customer cstomer;
 
 }
--- billing-service/src/main/java/com/soighiri/billing_service/entities/ProductItem.java
+++ billing-service/src/main/java/com/soighiri/billing_service/entities/ProductItem.java
@@ -1,17 +1,22 @@
 package com.soighiri.billing_service.entities;
 
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class ProductItem {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String productId;
+	@ManyToAny 
 	private Bill bill;
 	private int quantity;
 	private double unitPrice;
